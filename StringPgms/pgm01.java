d7c6c968cdd0930b0633b621f4e5ae7c5335ac8c
Amrut Rajendra Chandak <amrutc010@gmail.com>
2025-09-20 23:17:41 +0530

    My firstcommit

---
+++ StringPgms/pgm01.java
@@ -0,0 +1,9 @@
+public class pgm01 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello from pgm01!");
+        String actualWord = "Java"; 
+        System.out.println("The word is: " + actualWord);
+    }
+}
