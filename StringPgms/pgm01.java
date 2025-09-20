c4c09eb87dc6a4bd5123e1baa6b657a68ec999c8
Amrut Rajendra Chandak <amrutc010@gmail.com>
2025-09-20 23:32:42 +0530

    Pgm to check whether the word is palindrome or not

--- StringPgms/pgm01.java
+++ StringPgms/pgm01.java
@@ -7,5 +7,20 @@ public class pgm01
         System.out.println("Hello from pgm01!");
         String actualWord = "Java"; 
         System.out.println("The word is: " + actualWord);
+        String reversedWord = "";
+        char[] charArray = actualWord.toCharArray();
+        for(int i = charArray.length-1 ; i >= 0 ; i--)
+        {
+            reversedWord = reversedWord+charArray[i];
+        }
+        System.out.println("The reversed word is: " + reversedWord);
+        if(actualWord.equals(reversedWord))
+        {
+            System.out.println("The word is a palindrome");
+        }
+        else
+        {
+            System.out.println("The word is not a palindrome");
+        }
     }
 }
